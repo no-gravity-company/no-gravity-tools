4113308a01f50d5e27714ed37085d035c480e384
Jack Blakeston <jackblakeston93@gmail.com>
2023-03-04 16:36:42 +0100

    NGT-002: Update prettierrc

--- packages/tools/component-generator/src/index.ts
+++ packages/tools/component-generator/src/index.ts
@@ -2,19 +2,19 @@ import * as fsp from 'fs/promises';
 import * as path from 'path';
 
 const errorMessage = (message: string) => {
-  console.error(message);
-  process.exit(1);
+    console.error(message);
+    process.exit(1);
 };
 
 const getKebabCase = (name: string) => {
-  return name
-    .replace(/([a-z])([A-Z])/g, '$1-$2')
-    .replace(/[\s_]+/g, '-')
-    .toLocaleLowerCase();
+    return name
+        .replace(/([a-z])([A-Z])/g, '$1-$2')
+        .replace(/[\s_]+/g, '-')
+        .toLocaleLowerCase();
 };
 
 const getComponentTemplateWithValues = (componentName: string): string => {
-  return `import { Fragment, FunctionComponent, h } from 'preact';
+    return `import { Fragment, FunctionComponent, h } from 'preact';
 import { ${componentName}Props } from './types'
 
 const ${componentName}: FunctionComponent<${componentName}Props> = ({ x }: ${componentName}Props) => {
@@ -28,12 +28,11 @@ export default ${componentName};
 };
 
 const getGitIgnoreTemplate = (): string => {
-  return 'lib';
+    return 'lib';
 };
 
-const getPackageJsonTemplate = (componentName: string): string => {
-  const kebabCase = getKebabCase(componentName);
-  return `{
+const getPackageJsonTemplate = (kebabCase: string): string => {
+    return `{
     "name": "@no-gravity-elements/${kebabCase}",
     "version": "1.0.0",
     "description": "No Gravity Element",
@@ -62,73 +61,121 @@ const getPackageJsonTemplate = (componentName: string): string => {
 };
 
 const getTypesTemplate = (componentName: string): string => {
-  return `export interface ${componentName}Props {
+    return `export interface ${componentName}Props {
     x: string;
 }
     `;
 };
 
+const getStoryTemplate = (componentName: string, kebabCase: string): string => {
+    return `import { h } from 'preact';
+import './lib/index';
+
+export default {
+  title: 'My ${componentName}',
+  parameters: {
+    // Specify that the component is a Web Component
+    webComponents: {
+      // Set the tag name of the Web Component
+      tagName: 'nge-${kebabCase}',
+    },
+  },
+};
+
+export const Default = () => <nge-${kebabCase} />;
+    `;
+};
+
+const getRegisterTemplate = (
+    componentName: string,
+    kebabCase: string
+): string => {
+    return `import register from 'preact-custom-element';
+import ${componentName} from './${componentName}';
+
+register(${componentName}, 'nge-${kebabCase}', [], { shadow: true });
+    `;
+};
+
 const main = async (): Promise<void> => {
-  // Checks for --type and if it has a value
-  const componentTypeIndex = process.argv.indexOf('--type');
-  let componentTypeValue = '';
+    // Checks for --type and if it has a value
+    const componentTypeIndex = process.argv.indexOf('--type');
+    let componentTypeValue = '';
 
-  if (componentTypeIndex > -1) {
-    // Retrieve the value after --type
-    componentTypeValue = process.argv[componentTypeIndex + 1];
-    !componentTypeValue && errorMessage('No component type was provided');
-  } else {
-    errorMessage('No --type flag was provided');
-  }
+    if (componentTypeIndex > -1) {
+        // Retrieve the value after --type
+        componentTypeValue = process.argv[componentTypeIndex + 1];
+        !componentTypeValue && errorMessage('No component type was provided');
+    } else {
+        errorMessage('No --type flag was provided');
+    }
 
-  // Checks for --name and if it has a value
-  const componentNameIndex = process.argv.indexOf('--name');
-  let componentNameValue = '';
+    // Checks for --name and if it has a value
+    const componentNameIndex = process.argv.indexOf('--name');
+    let componentNameValue: string = '';
 
-  if (componentNameIndex > -1) {
-    // Retrieve the value after --name
-    componentNameValue = process.argv[componentNameIndex + 1];
-    !componentNameValue && errorMessage('No component name was provided');
-    // create component tsx file
-    const componentFolderPath = path.join(
-      'packages',
-      'components',
-      componentTypeValue,
-      componentNameValue
-    );
-    // TODO: use try and catch in case the component already exists
-    await fsp.mkdir(componentFolderPath);
-    // Component .tsx file
-    const componentFilePath = path.join(
-      componentFolderPath,
-      `${componentNameValue}.tsx`
-    );
-    const interpolation = getComponentTemplateWithValues(componentNameValue);
-    await fsp.writeFile(componentFilePath, interpolation, {
-      encoding: 'utf-8',
-    });
-    // .gitignore
-    const gitIgnorePath = path.join(componentFolderPath, '.gitignore');
-    const gitIgnoreTemplate = getGitIgnoreTemplate();
-    await fsp.writeFile(gitIgnorePath, gitIgnoreTemplate, {
-      encoding: 'utf-8',
-    });
-    // package.json
-    const packageJsonPath = path.join(componentFolderPath, 'package.json');
-    const packageJsonTemplate = getPackageJsonTemplate(componentNameValue);
-    await fsp.writeFile(packageJsonPath, packageJsonTemplate, {
-      encoding: 'utf-8',
-    });
-    // types
-    const typesFolderPath = path.join(componentFolderPath, 'types');
-    await fsp.mkdir(typesFolderPath);
-    const typesPath = path.join(typesFolderPath, 'index.ts');
-    const typesTemplate = getTypesTemplate(componentNameValue);
-    await fsp.writeFile(typesPath, typesTemplate, { encoding: 'utf-8' });
+    if (componentNameIndex > -1) {
+        // Retrieve the value after --name
+        componentNameValue = process.argv[componentNameIndex + 1];
+        !componentNameValue && errorMessage('No component name was provided');
+        // create component tsx file
+        const componentFolderPath = path.join(
+            'packages',
+            'components',
+            componentTypeValue,
+            componentNameValue
+        );
+        const kebabCase = getKebabCase(componentNameValue);
 
-    // TODO: unit-tests
-    // TODO: integration-tests
-  }
+        // TODO: use try and catch in case the component already exists
+        await fsp.mkdir(componentFolderPath);
+        // Component .tsx file
+        const componentFilePath = path.join(
+            componentFolderPath,
+            `${componentNameValue}.tsx`
+        );
+        const interpolation =
+            getComponentTemplateWithValues(componentNameValue);
+        await fsp.writeFile(componentFilePath, interpolation, {
+            encoding: 'utf-8',
+        });
+        // .gitignore
+        const gitIgnorePath = path.join(componentFolderPath, '.gitignore');
+        const gitIgnoreTemplate = getGitIgnoreTemplate();
+        await fsp.writeFile(gitIgnorePath, gitIgnoreTemplate, {
+            encoding: 'utf-8',
+        });
+        // package.json
+        const packageJsonPath = path.join(componentFolderPath, 'package.json');
+        const packageJsonTemplate = getPackageJsonTemplate(kebabCase);
+        await fsp.writeFile(packageJsonPath, packageJsonTemplate, {
+            encoding: 'utf-8',
+        });
+        // types
+        const typesFolderPath = path.join(componentFolderPath, 'types');
+        await fsp.mkdir(typesFolderPath);
+        const typesPath = path.join(typesFolderPath, 'index.ts');
+        const typesTemplate = getTypesTemplate(componentNameValue);
+        await fsp.writeFile(typesPath, typesTemplate, { encoding: 'utf-8' });
+        // story
+        const storyPath = path.join(
+            componentFolderPath,
+            `${componentNameValue}.stories.tsx`
+        );
+        const storyTemplate = getStoryTemplate(componentNameValue, kebabCase);
+        await fsp.writeFile(storyPath, storyTemplate, { encoding: 'utf-8' });
+        // register
+        const registerPath = path.join(componentFolderPath, 'register.ts');
+        const registerTemplate = getRegisterTemplate(
+            componentNameValue,
+            kebabCase
+        );
+        await fsp.writeFile(registerPath, registerTemplate, {
+            encoding: 'utf-8',
+        });
+        // TODO: unit-tests
+        // TODO: integration-tests
+    }
 };
 
 main();
